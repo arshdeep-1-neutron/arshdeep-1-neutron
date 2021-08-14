7bbf57e3ebe1569314828f4083e923b43f1b9509
arshdeep-1-neutron <88944435+arshdeep-1-neutron@users.noreply.github.com>
2021-08-14 23:43:26 +0530

    Add files via upload

---
+++ FINAL/src/js/form.js
@@ -0,0 +1,18 @@
+
+
+function revealHiring(){
+
+    let btn = document.querySelector("#hidden-hire");
+
+    btn.style.display="block";
+
+}
+
+
+function removeHiring(){
+
+    let btn = document.querySelector("#hidden-hire");
+
+    btn.style.display="none";
+
+}
